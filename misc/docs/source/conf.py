89261e53ca9aeeb9d964a89abe8eb1475f01246a
Tobias Megies <megies@geophysik.uni-muenchen.de>
2012-09-12 18:42:13 +0200

    print sys.path

--- misc/docs/source/conf.py
+++ misc/docs/source/conf.py
@@ -10,6 +10,9 @@ import os
 import sys
 
 
+import pprint
+pprint.pprint(sys.path)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
