75c0ebd89e5ec8f6292517281e117925dd2e8a30
rannof <ran.nof@gmail.com>
2016-06-09 15:15:03 -0700

    Fixed according to Flake8, fix for Güralp name.

--- obspy/io/gcf2mseed.py
+++ obspy/io/gcf2mseed.py
@@ -4,44 +4,52 @@
 # by Ran Nof @ BSL, 2016
 # ran.nof@gmail.com
 
-from gcf.core import _read_gcf as read_gcf
-from gcf.core import _is_gcf as is_gcf
-from glob import glob,sys
+import argparse
+import sys
+import textwrap
+
+from gcf.core import _is_gcf as is_gcf, _read_gcf as read_gcf
+
 from obspy import Stream
-import argparse,textwrap
+
 
 # set the argument parser
 parser = argparse.ArgumentParser(
-         formatter_class=argparse.RawDescriptionHelpFormatter,
-         description='''Convert Guralpe Compressed Format (GCF) to Miniseed''',
-         epilog=textwrap.dedent('''\
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='''Convert Güralp Compressed Format (GCF) to Miniseed''',
+    epilog=textwrap.dedent('''\
              See more details regarding GCF:
              http://www.guralp.com/apps/ok?doc=GCF_Intro
-    
+
              The script only converts data streams
-        
+
              Created by Ran Novitsky Nof @ BSL, 2016'''))
-parser.add_argument('-o',metavar='Output_file',type=str,help='Output Miniseed file name.')
-parser.add_argument('gcf_file',nargs='+',type=str,help='input file(s) name.')
-parser.add_argument('-O',metavar='option',type=str,nargs='*',default=["'MSEED'"],help='''\
-                         Options for the obspy write command (default: MSEED.)
-                         Example option:
-                         -O "format='MSEED'" encoding=11 reclen=512 byteorder=1.
-                         Note the " and ' around the format and MSEED values.'''
+parser.add_argument('-o', metavar='Output_file', type=str,
+                    help='Output Miniseed file name.')
+parser.add_argument('gcf_file', nargs='+', type=str,
+                    help='input file(s) name.')
+parser.add_argument('-O', metavar='option', type=str, nargs='*',
+                    default=["'MSEED'"], help='''
+                    Options for the obspy write command (default: MSEED.)
+                    Example option:
+                    -O "format='MSEED'" encoding=11 reclen=512 byteorder=1.
+                    Note the " and ' around the format and MSEED values.'''
                     )
 
-def getStream(FileList):
-  st = Stream()
-  for FileName in FileList:
-    if is_gcf(FileName):
-  		st += read_gcf(FileName)
-  return st 		
 
-def main(filesarg,output,writeArgs):
-  st = getStream(filesarg)
-  eval("st.write(output,%s)"%','.join(writeArgs))
+def getstream(filelist):
+    st = Stream()
+    for filename in filelist:
+        if is_gcf(filename):
+            st += read_gcf(filename)
+    return st
 
-if __name__=='__main__':
-  args = parser.parse_args(sys.argv[1:])
-  main(args.gcf_file,args.o,args.O)          
 
+def main(filesarg, output, writeargs):
+    st = getstream(filesarg)
+    eval("st.write(output,%s)" % ','.join(writeargs))
+
+
+if __name__ == '__main__':
+    args = parser.parse_args(sys.argv[1:])
+    main(args.gcf_file, args.o, args.O)
