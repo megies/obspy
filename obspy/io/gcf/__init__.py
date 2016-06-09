9bf32d35a6ce2a47941a38002f5883a95fbb391a
rannof <ran.nof@gmail.com>
2016-06-09 11:38:27 -0700

    Add read for Guralpe Compressed Format (GCF)
    Only read, no write.

---
+++ obspy/io/gcf/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+obspy.io.gcf - Guralp Compressed Format read support for ObsPy
+==============================================================
+This module provides read support for GCF waveform data and header info.
+Most methods are based on info from Guralp site
+http://geophysics.eas.gatech.edu/GTEQ/Scream4.4/GCF_Specification.htm
+
+:copyright:
+    The ObsPy Development Team (devs@obspy.org) & Ran Novitsky Nof
+:license:
+    GNU Lesser General Public License, Version 3
+    (https://www.gnu.org/copyleft/lesser.html)
+
+Reading
+-------
+Similar to reading any other waveform data format using :mod:`obspy.core`
+The format will be determined automatically.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(exclude_empty=True)
