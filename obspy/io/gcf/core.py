9405999a10a557b984e9360093d5bdfb6fe1a6d4
rannof <ran.nof@gmail.com>
2016-06-14 17:53:47 -0700

    Update core.py

--- obspy/io/gcf/core.py
+++ obspy/io/gcf/core.py
@@ -47,7 +47,7 @@ def _read_gcf(filename, headonly=False, **kwargs):  # @UnusedVariable
     :returns: Stream object containing header and data.
     .. rubric:: Example
     >>> from obspy import read
-    >>> st = read("/path/to/20160603_1955n.gcf")
+    >>> st = read("/path/to/20160603_1955n.gcf",format="GCF")
     """
     traces = []
     with open(filename, 'rb') as f:
