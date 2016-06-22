f4499e49ba8bb5d04d5ce8511b423d515d2e3345
Ran Nof <ran.nof@gmail.com>
2016-06-22 09:48:45 -0700

    test headonly

--- obspy/io/gcf/tests/test_core.py
+++ obspy/io/gcf/tests/test_core.py
@@ -10,13 +10,16 @@ from future.builtins import *  # NOQA
 import os
 import unittest
 
+import numpy as np
+
 from obspy import read
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.io.gcf.core import _read_gcf
 
-EXPECTED = array([-49378, -49213, -49273, -49277, -49341, -49415, -49289,
-                  -49309, -49277, -49381, -49441, -49276, -49331, -49268,
-                  -49250, -49407, -49421, -49282, -49224, -49281], dtype=int32)
+EXPECTED = np.array([-49378, -49213, -49273, -49277, -49341, -49415, -49289,
+                     -49309, -49277, -49381, -49441, -49276, -49331, -49268,
+                     -49250, -49407, -49421, -49282, -49224, -49281],
+                    dtype=np.int32)
 
 
 class CoreTestCase(unittest.TestCase):
@@ -46,6 +49,23 @@ class CoreTestCase(unittest.TestCase):
         self.assertEqual(st[0].stats.station, '6018')
         np.testing.assert_array_equal(EXPECTED, st[0].data[:20])
 
+    def test_read_head_via_obspy(self):
+        """
+        Read files via obspy.core.stream.read function.
+        """
+        filename = os.path.join(self.path, '20160603_1955n.gcf')
+        # 1
+        st = read(filename, headonly=True)
+        self.assertEqual(len(st), 2)
+        self.assertEqual(st[0].stats.starttime,
+                         UTCDateTime('2016-06-03T19:55:00.000000Z'))
+        self.assertEqual(st[0].stats.endtime,
+                         UTCDateTime('2016-06-03T19:55:01.990000Z'))
+        self.assertEqual(len(st[0]), 200)
+        self.assertAlmostEqual(st[0].stats.sampling_rate, 100.0)
+        self.assertEqual(st[0].stats.channel, 'HHN')
+        self.assertEqual(st[0].stats.station, '6018')
+
     def test_read_via_module(self):
         """
         Read files via obspy.io.gcf.core._read_gcf function.
@@ -64,6 +84,23 @@ class CoreTestCase(unittest.TestCase):
         self.assertEqual(st[0].stats.channel, 'HHN')
         self.assertEqual(st[0].stats.station, '6018')
         np.testing.assert_array_equal(EXPECTED, st[0].data[:20])
+
+    def test_read_head_via_module(self):
+        """
+        Read files via obspy.io.gcf.core._read_gcf function.
+        """
+        filename = os.path.join(self.path, '20160603_1955n.gcf')
+        # 1
+        st = _read_gcf(filename, headonly=True)
+        self.assertEqual(len(st), 1)
+        self.assertEqual(st[0].stats.starttime,
+                         UTCDateTime('2016-06-03T19:55:00.000000Z'))
+        self.assertEqual(st[0].stats.endtime,
+                         UTCDateTime('2016-06-03T19:55:01.990000Z'))
+        self.assertEqual(len(st[0]), 200)
+        self.assertAlmostEqual(st[0].stats.sampling_rate, 100.0)
+        self.assertEqual(st[0].stats.channel, 'HHN')
+        self.assertEqual(st[0].stats.station, '6018')
 
 
 def suite():
