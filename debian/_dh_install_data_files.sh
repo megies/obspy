873db3f922a2121c04600e9a7160eb00bb01e8bb
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-10-13 11:26:21 +0200

    try

--- debian/_dh_install_data_files.sh
+++ debian/_dh_install_data_files.sh
@@ -8,4 +8,5 @@ do
     dh_install -p python-obspy-dbg ${TESTSDIR}/* ${TARGET}
 done
 dh_installdirs -p python-obspy-dbg usr/share/obspy/io/mseed/tests/data/libmseed
-dh_install -p python-obspy-dbg ./debian/tmp/usr/lib/python2.7/dist-packages/obspy/io/mseed/src/libmseed/test/* usr/share/obspy/io/mseed/tests/data/libmseed
+dh_install -p python-obspy-dbg ./debian/tmp/usr/lib/python2.7/dist-packages/obspy/io/mseed/src/libmseed/test/*.ref usr/share/obspy/io/mseed/tests/data/libmseed
+dh_install -p python-obspy-dbg ./debian/tmp/usr/lib/python2.7/dist-packages/obspy/io/mseed/src/libmseed/test/*.test usr/share/obspy/io/mseed/tests/data/libmseed
