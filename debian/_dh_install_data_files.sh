1b63da4816a1cc892ad81f4c645b35cb070a4ea2
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-10-13 10:58:03 +0200

    DELETE/SQUASH

--- debian/_dh_install_data_files.sh
+++ debian/_dh_install_data_files.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-for TESTSDIR in `find . -type d -regextype posix-egrep -regex './debian/tmp/usr/lib/python2.7/dist-packages/obspy/.*/tests/(data|images)'` ./debian/tmp/usr/lib/python2.7/dist-packages/obspy/io/mseed/src/libmseed/test/data
+for TESTSDIR in `find . -type d -regextype posix-egrep -regex './debian/tmp/usr/lib/python2.7/dist-packages/obspy/.*/tests/(data|images)'`
 do
     SUFFIX=`echo $TESTSDIR | sed 's#.*-packages/##'`
     TARGET=usr/share/${SUFFIX}
     dh_installdirs -p python-obspy-dbg ${TARGET}
     dh_install -p python-obspy-dbg ${TESTSDIR}/* ${TARGET}
 done
-dh_installdirs -p python-obspy-dbg -A usr/share/obspy/io/mseed/src/libmseed/test/data
